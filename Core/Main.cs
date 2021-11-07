c70064e50442ba920b3c63ba82c67eb4c5c62994
liwei1dao <liwei1dao@gmail.com>
2021-11-07 14:42:05 +0800

    替换日志插件

--- Core/Main.cs
+++ Core/Main.cs
@@ -4,46 +4,32 @@ using System.IO;
 
 namespace lgu3d
 {
-    public abstract class Main : MonoBehaviour
+  public abstract class Main : MonoBehaviour
+  {
+    [MFWAttributeRename("是否开启版本检测")]
+    public bool IsOpenVersionCheck;
+    [MFWAttributeRename("App资源加载方式")]
+    public AppResModel AppResModel;
+    protected virtual void SetConfig()
     {
-        [MFWAttributeRename("是否显示日志")]
-        public bool IsShowLog;
-        [MFWAttributeRename("是否开启版本检测")]
-        public bool IsOpenVersionCheck;
-        [MFWAttributeRename("App资源加载方式")]
-        public AppResModel AppResModel;
-        protected virtual void SetConfig()
-        {
-            if (IsShowLog) {
-                string logpath = string.Format(AppConfig.AppLogAddress + "/{0}.log", string.Format("{0:yyyy年MM月dd日HH时mm分ss秒}", DateTime.Now));
-                if (!Directory.Exists(AppConfig.AppLogAddress))
-                {
-                    Directory.CreateDirectory(AppConfig.AppLogAddress);
-                }
-                File.Create(logpath).Dispose();
-                HiLog.HiLogTextPath = logpath;
-                HiLog.SetOn(IsShowLog);
-                Debug.Log("日志地址:" + logpath);
-            }
-            AppConfig.IsOpenVersionCheck = IsOpenVersionCheck;
-            AppConfig.AppResModel = AppResModel;
-
+      AppConfig.IsOpenVersionCheck = IsOpenVersionCheck;
+      AppConfig.AppResModel = AppResModel;
 #if UNITY_IOS
             AppConfig.TargetPlatform = AppPlatform.IOS;
 #elif UNITY_ANDROID
-            AppConfig.TargetPlatform = AppPlatform.Android;
+      AppConfig.TargetPlatform = AppPlatform.Android;
 #else
             AppConfig.TargetPlatform = AppPlatform.Windows;
 #endif
-        }
-
-        private void Awake()
-        {
-            SetConfig();
-            StartApp();
-        }
-
-        protected abstract void StartApp();
     }
+
+    private void Awake()
+    {
+      SetConfig();
+      StartApp();
+    }
+
+    protected abstract void StartApp();
+  }
 }
 
