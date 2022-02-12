b2b15f4c98147986c08ad96caff485b20b068957
liwei1dao <liwei1dao@gmail.com>
2022-02-12 16:49:27 +0800

    上传GridLayoutGroupAnim 代码优化

--- Extend/UI/GridLayoutGroup/GridLayoutGroupAnim.cs
+++ Extend/UI/GridLayoutGroup/GridLayoutGroupAnim.cs
@@ -4,203 +4,209 @@ using UnityEngine.UI;
 
 namespace lgu3d
 {
-    /// <summary>
-    /// 布局动画组件
-    /// </summary>
-    public class GridLayoutGroupAnim:GridLayoutGroup{
-        [SerializeField] protected float m_animTime = 0.3f;
-        public float animTime { get { return m_animTime; } set { SetProperty(ref m_animTime, value); } }
-        
-        public override void CalculateLayoutInputHorizontal()
-        {
-            base.CalculateLayoutInputHorizontal();
+  /// <summary>
+  /// 布局动画组件
+  /// </summary>
+  public class GridLayoutGroupAnim : GridLayoutGroup
+  {
+    [SerializeField] protected float m_animTime = 0.3f;
+    public float animTime { get { return m_animTime; } set { SetProperty(ref m_animTime, value); } }
 
-            int minColumns = 0;
-            int preferredColumns = 0;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                minColumns = preferredColumns = m_ConstraintCount;
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                minColumns = preferredColumns = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
-            }
-            else
-            {
-                minColumns = 1;
-                preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(rectChildren.Count));
-            }
+    public override void CalculateLayoutInputHorizontal()
+    {
+      base.CalculateLayoutInputHorizontal();
 
-            SetLayoutInputForAxis(
-                padding.horizontal + (cellSize.x + spacing.x) * minColumns - spacing.x,
-                padding.horizontal + (cellSize.x + spacing.x) * preferredColumns - spacing.x,
-                -1, 0);
-        }
+      int minColumns = 0;
+      int preferredColumns = 0;
+      if (m_Constraint == Constraint.FixedColumnCount)
+      {
+        minColumns = preferredColumns = m_ConstraintCount;
+      }
+      else if (m_Constraint == Constraint.FixedRowCount)
+      {
+        minColumns = preferredColumns = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
+      }
+      else
+      {
+        minColumns = 1;
+        preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(rectChildren.Count));
+      }
 
-        public override void CalculateLayoutInputVertical()
-        {
-            int minRows = 0;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                minRows = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                minRows = m_ConstraintCount;
-            }
-            else
-            {
-                float width = rectTransform.rect.size.x;
-                int cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
-                minRows = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX);
-            }
-
-            float minSpace = padding.vertical + (cellSize.y + spacing.y) * minRows - spacing.y;
-            SetLayoutInputForAxis(minSpace, minSpace, -1, 1);
-        }
-
-        public override void SetLayoutHorizontal()
-        {
-            SetCellsAlongAxis(0);
-        }
-
-        public override void SetLayoutVertical()
-        {
-            SetCellsAlongAxis(1);
-        }
-
-        private void SetCellsAlongAxis(int axis)
-        {
-            // Normally a Layout Controller should only set horizontal values when invoked for the horizontal axis
-            // and only vertical values when invoked for the vertical axis.
-            // However, in this case we set both the horizontal and vertical position when invoked for the vertical axis.
-            // Since we only set the horizontal position and not the size, it shouldn't affect children's layout,
-            // and thus shouldn't break the rule that all horizontal layout must be calculated before all vertical layout.
-
-            if (axis == 0)
-            {
-                // Only set the sizes when invoked for horizontal axis, not the positions.
-                for (int i = 0; i < rectChildren.Count; i++)
-                {
-                    RectTransform rect = rectChildren[i];
-
-                    m_Tracker.Add(this, rect,
-                        DrivenTransformProperties.Anchors |
-                        DrivenTransformProperties.AnchoredPosition |
-                        DrivenTransformProperties.SizeDelta);
-
-                    rect.anchorMin = Vector2.up;
-                    rect.anchorMax = Vector2.up;
-                    rect.sizeDelta = cellSize;
-                }
-                return;
-            }
-
-            float width = rectTransform.rect.size.x;
-            float height = rectTransform.rect.size.y;
-
-            int cellCountX = 1;
-            int cellCountY = 1;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                cellCountX = m_ConstraintCount;
-                cellCountY = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX - 0.001f);
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                cellCountY = m_ConstraintCount;
-                cellCountX = Mathf.CeilToInt(rectChildren.Count / (float)cellCountY - 0.001f);
-            }
-            else
-            {
-                cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
-                cellCountY = Mathf.Max(1, Mathf.FloorToInt((height - padding.vertical + spacing.y + 0.001f) / (cellSize.y + spacing.y)));
-            }
-
-            int cornerX = (int)startCorner % 2;
-            int cornerY = (int)startCorner / 2;
-
-            int cellsPerMainAxis, actualCellCountX, actualCellCountY;
-            if (startAxis == Axis.Horizontal)
-            {
-                cellsPerMainAxis = cellCountX;
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, rectChildren.Count);
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
-            }
-            else
-            {
-                cellsPerMainAxis = cellCountY;
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, rectChildren.Count);
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
-            }
-
-            Vector2 requiredSpace = new Vector2(
-                    actualCellCountX * cellSize.x + (actualCellCountX - 1) * spacing.x,
-                    actualCellCountY * cellSize.y + (actualCellCountY - 1) * spacing.y
-                    );
-            Vector2 startOffset = new Vector2(
-                    GetStartOffset(0, requiredSpace.x),
-                    GetStartOffset(1, requiredSpace.y)
-                    );
-
-            for (int i = 0; i < rectChildren.Count; i++)
-            {
-                int positionX;
-                int positionY;
-                if (startAxis == Axis.Horizontal)
-                {
-                    positionX = i % cellsPerMainAxis;
-                    positionY = i / cellsPerMainAxis;
-                }
-                else
-                {
-                    positionX = i / cellsPerMainAxis;
-                    positionY = i % cellsPerMainAxis;
-                }
-
-                if (cornerX == 1)
-                    positionX = actualCellCountX - 1 - positionX;
-                if (cornerY == 1)
-                    positionY = actualCellCountY - 1 - positionY;
-                StartCoroutine(SetChildAnim(rectChildren[i],new Vector2(startOffset.x + (cellSize[0] + spacing[0]) * positionX,startOffset.y + (cellSize[1] + spacing[1]) * positionY),new Vector2(cellSize[0],cellSize[1])));
-                // SetChildAlongAxis(rectChildren[i], 0, startOffset.x + (cellSize[0] + spacing[0]) * positionX, cellSize[0]);
-                // SetChildAlongAxis(rectChildren[i], 1, startOffset.y + (cellSize[1] + spacing[1]) * positionY, cellSize[1]);
-            }
-        }
-
-        // protected void SetChildAlongAxis(RectTransform rect, int axis, float pos, float size)
-        // {
-        //     if (rect == null)
-        //         return;
-
-        //     m_Tracker.Add(this, rect,
-        //         DrivenTransformProperties.Anchors |
-        //         DrivenTransformProperties.AnchoredPosition |
-        //         DrivenTransformProperties.SizeDelta);
-
-        //     rect.SetInsetAndSizeFromParentEdge(axis == 0 ? RectTransform.Edge.Left : RectTransform.Edge.Top, pos, size);
-        // }
-
-        private IEnumerator SetChildAnim(RectTransform rect,Vector2 pos,Vector2 size){
-            float lefttime = 0;
-            float progress = 0;
-            Vector2 _pos = rect.anchoredPosition;
-            Vector2 _siez = rect.sizeDelta;
-            m_Tracker.Add(this, rect,
-                DrivenTransformProperties.Anchors |
-                DrivenTransformProperties.AnchoredPosition |
-                DrivenTransformProperties.SizeDelta);
-            while(lefttime < animTime){
-                lefttime += Time.deltaTime;
-                progress = lefttime/animTime;
-                Vector2 _temppos = Vector2.Lerp(_pos,pos,progress);
-                Vector2 _tempsize = Vector2.Lerp(_siez,size,progress);
-                rect.SetInsetAndSizeFromParentEdge( RectTransform.Edge.Left , _temppos.x, _tempsize.x);
-                rect.SetInsetAndSizeFromParentEdge( RectTransform.Edge.Top , _temppos.y, _tempsize.y);
-                yield return 1;
-            }
-            rect.SetInsetAndSizeFromParentEdge( RectTransform.Edge.Left , pos.x, size.x);
-            rect.SetInsetAndSizeFromParentEdge( RectTransform.Edge.Top , pos.y, size.y);
-        }
+      SetLayoutInputForAxis(
+          padding.horizontal + (cellSize.x + spacing.x) * minColumns - spacing.x,
+          padding.horizontal + (cellSize.x + spacing.x) * preferredColumns - spacing.x,
+          -1, 0);
     }
+
+    public override void CalculateLayoutInputVertical()
+    {
+      int minRows = 0;
+      if (m_Constraint == Constraint.FixedColumnCount)
+      {
+        minRows = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
+      }
+      else if (m_Constraint == Constraint.FixedRowCount)
+      {
+        minRows = m_ConstraintCount;
+      }
+      else
+      {
+        float width = rectTransform.rect.size.x;
+        int cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
+        minRows = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX);
+      }
+
+      float minSpace = padding.vertical + (cellSize.y + spacing.y) * minRows - spacing.y;
+      SetLayoutInputForAxis(minSpace, minSpace, -1, 1);
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+      SetCellsAlongAxis(0);
+    }
+
+    public override void SetLayoutVertical()
+    {
+      SetCellsAlongAxis(1);
+    }
+
+    private void SetCellsAlongAxis(int axis)
+    {
+      // Normally a Layout Controller should only set horizontal values when invoked for the horizontal axis
+      // and only vertical values when invoked for the vertical axis.
+      // However, in this case we set both the horizontal and vertical position when invoked for the vertical axis.
+      // Since we only set the horizontal position and not the size, it shouldn't affect children's layout,
+      // and thus shouldn't break the rule that all horizontal layout must be calculated before all vertical layout.
+
+      if (axis == 0)
+      {
+        // Only set the sizes when invoked for horizontal axis, not the positions.
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+          RectTransform rect = rectChildren[i];
+
+          m_Tracker.Add(this, rect,
+              DrivenTransformProperties.Anchors |
+              DrivenTransformProperties.AnchoredPosition |
+              DrivenTransformProperties.SizeDelta);
+
+          rect.anchorMin = Vector2.up;
+          rect.anchorMax = Vector2.up;
+          rect.sizeDelta = cellSize;
+        }
+        return;
+      }
+
+      float width = rectTransform.rect.size.x;
+      float height = rectTransform.rect.size.y;
+
+      int cellCountX = 1;
+      int cellCountY = 1;
+      if (m_Constraint == Constraint.FixedColumnCount)
+      {
+        cellCountX = m_ConstraintCount;
+        cellCountY = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX - 0.001f);
+      }
+      else if (m_Constraint == Constraint.FixedRowCount)
+      {
+        cellCountY = m_ConstraintCount;
+        cellCountX = Mathf.CeilToInt(rectChildren.Count / (float)cellCountY - 0.001f);
+      }
+      else
+      {
+        cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
+        cellCountY = Mathf.Max(1, Mathf.FloorToInt((height - padding.vertical + spacing.y + 0.001f) / (cellSize.y + spacing.y)));
+      }
+
+      int cornerX = (int)startCorner % 2;
+      int cornerY = (int)startCorner / 2;
+
+      int cellsPerMainAxis, actualCellCountX, actualCellCountY;
+      if (startAxis == Axis.Horizontal)
+      {
+        cellsPerMainAxis = cellCountX;
+        actualCellCountX = Mathf.Clamp(cellCountX, 1, rectChildren.Count);
+        actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
+      }
+      else
+      {
+        cellsPerMainAxis = cellCountY;
+        actualCellCountY = Mathf.Clamp(cellCountY, 1, rectChildren.Count);
+        actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
+      }
+
+      Vector2 requiredSpace = new Vector2(
+              actualCellCountX * cellSize.x + (actualCellCountX - 1) * spacing.x,
+              actualCellCountY * cellSize.y + (actualCellCountY - 1) * spacing.y
+              );
+      Vector2 startOffset = new Vector2(
+              GetStartOffset(0, requiredSpace.x),
+              GetStartOffset(1, requiredSpace.y)
+              );
+
+      for (int i = 0; i < rectChildren.Count; i++)
+      {
+        int positionX;
+        int positionY;
+        if (startAxis == Axis.Horizontal)
+        {
+          positionX = i % cellsPerMainAxis;
+          positionY = i / cellsPerMainAxis;
+        }
+        else
+        {
+          positionX = i / cellsPerMainAxis;
+          positionY = i % cellsPerMainAxis;
+        }
+
+        if (cornerX == 1)
+          positionX = actualCellCountX - 1 - positionX;
+        if (cornerY == 1)
+          positionY = actualCellCountY - 1 - positionY;
+        StartCoroutine(SetChildAnim(rectChildren[i], new Vector2(startOffset.x + (cellSize[0] + spacing[0]) * positionX, startOffset.y + (cellSize[1] + spacing[1]) * positionY), new Vector2(cellSize[0], cellSize[1])));
+        // SetChildAlongAxis(rectChildren[i], 0, startOffset.x + (cellSize[0] + spacing[0]) * positionX, cellSize[0]);
+        // SetChildAlongAxis(rectChildren[i], 1, startOffset.y + (cellSize[1] + spacing[1]) * positionY, cellSize[1]);
+      }
+    }
+
+    // protected void SetChildAlongAxis(RectTransform rect, int axis, float pos, float size)
+    // {
+    //     if (rect == null)
+    //         return;
+
+    //     m_Tracker.Add(this, rect,
+    //         DrivenTransformProperties.Anchors |
+    //         DrivenTransformProperties.AnchoredPosition |
+    //         DrivenTransformProperties.SizeDelta);
+
+    //     rect.SetInsetAndSizeFromParentEdge(axis == 0 ? RectTransform.Edge.Left : RectTransform.Edge.Top, pos, size);
+    // }
+
+    private IEnumerator SetChildAnim(RectTransform rect, Vector2 pos, Vector2 size)
+    {
+      float lefttime = 0;
+      float progress = 0;
+      Vector2 _pos = new Vector2(rect.offsetMin.x, -rect.offsetMax.y);
+      Vector2 _siez = rect.sizeDelta;
+      m_Tracker.Add(this, rect,
+          DrivenTransformProperties.Anchors |
+          DrivenTransformProperties.AnchoredPosition |
+          DrivenTransformProperties.SizeDelta);
+      while (rect != null && lefttime < animTime)
+      {
+        lefttime += Time.deltaTime;
+        progress = lefttime / animTime;
+        Vector2 _temppos = Vector2.Lerp(_pos, pos, progress);
+        Vector2 _tempsize = Vector2.Lerp(_siez, size, progress);
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, _temppos.x, _tempsize.x);
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, _temppos.y, _tempsize.y);
+        yield return 1;
+      }
+      if (rect != null)
+      {
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, pos.x, size.x);
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, pos.y, size.y);
+      }
+    }
+  }
 }
